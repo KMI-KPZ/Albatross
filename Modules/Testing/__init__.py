69ae9cc005e1a6283613e352b82a9183ac3e2106
Stephan Bischoff <stephan.bischoff@digle.net>
2017-10-26 14:11:30 +0200

    updated platform to python3
    + added __pycache__ directorys to the .gitignore

--- Modules/Testing/__init__.py
+++ Modules/Testing/__init__.py
@@ -10,4 +10,4 @@ from bokeh.models import Button, CustomJS
 from bokeh.palettes import RdYlBu3
 from bokeh.models.widgets import Select, TextInput, Div
 from bokeh.plotting import figure, curdoc, reset_output
-from bokeh.io.doc import set_curdoc
+from bokeh.io import set_curdoc
--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 import importlib
 
-
 menu = importlib.import_module('Modules.Menu.menu') 
 menu.define_menu()
