895d6a811a34a22d4fd8f7f8c217b8de66bc9e5f
Stephan Bischoff <stephan.bischoff@digle.net>
2017-11-10 17:27:49 +0100

    Added Endpoint to GeoJSON

--- Modules/02_DataProcessing/main.py
+++ Modules/02_DataProcessing/main.py
@@ -1,26 +1,14 @@
-import urllib
 import json
 import copy
 import rdflib as rdf
+from SPARQLWrapper import SPARQLWrapper, JSON
 import os
 import subprocess
-import xml.etree.ElementTree
-import lxml.etree as le
-import gzip
-from lxml import etree
-from bokeh.application.handlers import FunctionHandler
-from functools import partial
-from html5lib.constants import namespaces
-from bokeh.models import Button, CustomJS, Div, MultiSelect, Paragraph
-from bokeh.layouts import column, widgetbox, row
-import spq_plat
-from numpy.core import multiarray
-from bokeh.core.properties import Enum
+from bokeh.models import Button, Paragraph
+from bokeh.layouts import column, widgetbox
 from bokeh.models.sources import ColumnDataSource
 from bokeh.models.widgets.tables import TableColumn, DataTable
-from bokeh.models.widgets.groups import CheckboxGroup
-from bokeh.core.enums import enumeration
-from bokeh.plotting import curdoc
+from bokeh.models.widgets.inputs import TextInput
 
 
 class DataProcessing():
@@ -148,20 +136,22 @@ class DataProcessing():
         """
         converter = RDFToGeoJSON(self.layout) 
  
-        convert_button = Button(label="Convert to GeoJSON", button_type="success") 
-        convert_button.on_click(converter.transform) 
- 
-        self.layout.children[1] = column(widgetbox(converter.rdf_table), convert_button) 
-        # s.layout.children[2] = column(converter.geojson_table) 
-     
-        geojson_table_source = ColumnDataSource(data) 
-        columns = [TableColumn(field='lvl', title="NUTS Level"), 
-                   TableColumn(field='id', title="ID")] 
-        geojson_data_table = DataTable(source=geojson_table_source, columns=columns, width=500, height=400, selectable=True) 
-        #layout 
-        #s.layout.children[1] = column(widgetbox(data_table), width=500) 
-        self.layout.children[2] = column(widgetbox(geojson_data_table), width=500) 
-    
+        convert_button = Button(label="Convert to GeoJSON", button_type="success")
+        convert_button.on_click(converter.transform)
+
+        self.layout.children[1] = column(widgetbox(converter.rdf_table), convert_button)
+        # # s.layout.children[2] = column(converter.geojson_table)
+        #
+        # geojson_table_source = ColumnDataSource(data)
+        # columns = [TableColumn(field='lvl', title="NUTS Level"),
+        #            TableColumn(field='id', title="ID")]
+        # geojson_data_table = DataTable(source=geojson_table_source, columns=columns, width=500, height=400, selectable=True)
+        # #layout
+        # #s.layout.children[1] = column(widgetbox(data_table), width=500)
+        # self.layout.children[2] = column(widgetbox(geojson_data_table), width=500)
+
+    def endpoint_to_geojson(self):
+        converter = EndpointToGeoJSON(self.layout)
 
 class RDFToGeoJSON:
     def __init__(self, layout):
@@ -207,7 +197,7 @@ class RDFToGeoJSON:
         )
         convert_button = Button(label="Convert to GeoJSON", button_type="success")
         convert_button.on_click(self.transform)
-        #todo:creates bug - empty table
+        # todo: creates bug - empty table
         #self.__layout.children[1] = column(widgetbox(self.rdf_table), convert_button)
         self.__layout.children[2] = column(self.geojson_table)
 
@@ -378,3 +368,214 @@ class RDFToGeoJSON:
                     nuts[nuts_lvl]['features'][index]['properties']['OBSERVATIONS'] = {
                         file['id']: [observation]
                     }
+
+class EndpointToGeoJSON:
+    def __init__(self, layout):
+        self.__layout = layout
+        self.__selected = []
+        self.__file_list = self.__get_eurostats()
+
+        geojson_data = {'id': [], 'lvl': []}
+        for file in self.__file_list:
+            if file['geojson']['nuts1']['exists']:
+                geojson_data['id'].append(file['id'])
+                geojson_data['lvl'].append(1)
+
+            if file['geojson']['nuts2']['exists']:
+                geojson_data['id'].append(file['id'])
+                geojson_data['lvl'].append(2)
+
+            if file['geojson']['nuts3']['exists']:
+                geojson_data['id'].append(file['id'])
+                geojson_data['lvl'].append(3)
+        self.__geojson_table_source = ColumnDataSource(geojson_data)
+        geojson_table_columns = [
+            TableColumn(field='lvl', title='NUTS Level'),
+            TableColumn(field='id', title='ID')
+        ]
+        self.geojson_table = DataTable(
+            source=self.__geojson_table_source,
+            columns=geojson_table_columns,
+            width=300,
+            height=500,
+            selectable=True
+        )
+
+        convert_button = Button(label="Convert to GeoJSON", button_type="success")
+        convert_button.on_click(self.transform)
+        self.__endpoint_input = TextInput(placeholder="SPARQL Endpoint")
+        self.__graph_input = TextInput(placeholder="Graph")
+        self.__layout.children[1] = column(self.__endpoint_input, self.__graph_input, convert_button)
+        self.__layout.children[2] = column(self.geojson_table)
+
+
+    def transform(self):
+        if self.__graph_input.value and self.__endpoint_input.value:
+            filename = self.__graph_input.value.split('/')[-1].split('.')[0]
+
+            print("converting")
+            sparql = SPARQLWrapper("https://www.foodie-cloud.org/sparql")
+            query_a = """
+            prefix obs: <http://purl.org/linked-data/sdmx/2009/measure#>
+            prefix prop: <http://eurostat.linked-statistics.org/property#>
+            prefix qb: <http://purl.org/linked-data/cube#>
+            prefix sdmx-dimension: <http://purl.org/linked-data/sdmx/2009/dimension#>
+            
+            select distinct ?designation ?time ?value ?unit
+            """
+
+            graph = "from <{}>".format(self.__graph_input.value)
+
+            query_b = """
+            where {
+                ?observation a qb:Observation.
+                ?observation prop:geo ?designation.
+                ?observation prop:unit ?unit.
+                ?observation sdmx-dimension:timePeriod ?time.
+                ?observation obs:obsValue ?value.
+            }
+            """
+
+            query = "{}\n{}\n{}".format(query_a, graph, query_b)
+
+            sparql.setQuery(query)
+            sparql.setReturnFormat(JSON)
+            results = sparql.query().convert()
+
+            print("writing")
+            self.__write_geojson(results, filename)
+            print("done converting")
+
+            self.__file_list = self.__get_eurostats()
+            geojson_data = {'id': [], 'lvl': []}
+            for file in self.__file_list:
+                if file['geojson']['nuts1']['exists']:
+                    geojson_data['id'].append(file['id'])
+                    geojson_data['lvl'].append(1)
+
+                if file['geojson']['nuts2']['exists']:
+                    geojson_data['id'].append(file['id'])
+                    geojson_data['lvl'].append(2)
+
+                if file['geojson']['nuts3']['exists']:
+                    geojson_data['id'].append(file['id'])
+                    geojson_data['lvl'].append(3)
+            self.__geojson_table_source = ColumnDataSource(geojson_data)
+            geojson_table_columns = [
+                TableColumn(field='lvl', title='NUTS Level'),
+                TableColumn(field='id', title='ID')
+            ]
+            self.geojson_table = DataTable(
+                source=self.__geojson_table_source,
+                columns=geojson_table_columns,
+                width=300,
+                height=500,
+                selectable=True
+            )
+            self.__layout.children[2] = column(self.geojson_table)
+
+    def __write_geojson(self, results, filename):
+
+        with open('data/geojson/eurostats/nuts_rg_60M_2013_lvl_3.geojson') as f:
+            nuts3 = json.load(f)
+        with open('data/geojson/eurostats/nuts_rg_60M_2013_lvl_2.geojson') as f:
+            nuts2 = json.load(f)
+        with open('data/geojson/eurostats/nuts_rg_60M_2013_lvl_1.geojson') as f:
+            nuts1 = json.load(f)
+
+        nuts = [nuts1, nuts2, nuts3]
+
+        geojson = copy.deepcopy(nuts)
+        self.__process_file(results, geojson, filename)
+        base_path = os.path.join("data", "geojson", "eurostats")
+
+        for lvl in range(0, len(geojson)):
+            path = str(os.path.join(base_path, "nuts_{}".format(lvl +1), "{}.geojson".format(filename)))
+            with open(path, 'w') as outfile:
+                json.dump(geojson[lvl], fp=outfile, indent=4)
+
+    def __process_file(self, results, nuts, filename):
+        for result in results["results"]["bindings"]:
+            geo = result['designation']['value'].split('#')[-1]
+            time = result['time']['value']
+            value = result['value']['value']
+            unit = result['unit']['value'].split('#')[-1]
+
+            # search for the NUTS_ID (geo) in the NUTS level 1 to 3
+            index = -1
+            nuts_lvl = -1
+            found = False
+            while not found:
+                index += 1
+                done = []
+                # prepare break condition
+                for lvl in range(0, len(nuts)):
+                    done.append(False)
+
+                # check if the ID matches in any of the NUTS levels
+                for lvl in range(0, len(nuts)):
+                    if index < len(nuts[lvl]['features']):
+                        if nuts[lvl]['features'][index]['properties']['NUTS_ID'] == geo:
+                            nuts_lvl = lvl
+                            found = True
+                            break
+                    else:
+                        done[lvl] = True
+                if all(done):
+                    break
+
+            if nuts_lvl != -1:
+                observation = {
+                    'period': time,
+                    'unit': unit,
+                    'value': value
+                }
+                # check if any of the nested elements in the JSON already exist
+                if 'OBSERVATIONS' in nuts[nuts_lvl]['features'][index]['properties']:
+                    if filename in nuts[nuts_lvl]['features'][index]['properties']['OBSERVATIONS']:
+                        duplicate = False
+                        for observations in nuts[nuts_lvl]['features'][index]['properties']['OBSERVATIONS'][filename]:
+                            if observations['period'] == observation['period']:
+                                duplicate = True
+                                break
+                        if not duplicate:
+                            nuts[nuts_lvl]['features'][index]['properties']['OBSERVATIONS'][filename].append(
+                                observation)
+                    else:
+                        nuts[nuts_lvl]['features'][index]['properties']['OBSERVATIONS'][filename] = [observation]
+                else:
+                    nuts[nuts_lvl]['features'][index]['properties']['OBSERVATIONS'] = {
+                        filename: [observation]
+                    }
+
+    def __get_eurostats(self):
+        """
+        This function generates the file names for every RDF in the "data/rdf/eurostats" subdirectory.
+
+        :return: A list of dictionaries containing the id and file names of the RDFs found.
+        """
+        rdf_path_prefix = "data/rdf/eurostats/"
+        geojson_path_prefix = "data/geojson/eurostats/"
+        observation_list = []
+
+        nuts_files = [
+            [str(os.path.basename(file).split('.')[0]) for file in os.listdir(geojson_path_prefix + "nuts_1/")],
+            [str(os.path.basename(file).split('.')[0]) for file in os.listdir(geojson_path_prefix + "nuts_2/")],
+            [str(os.path.basename(file).split('.')[0]) for file in os.listdir(geojson_path_prefix + "nuts_3/")]
+        ]
+
+        for file in os.listdir(rdf_path_prefix):
+            observation = {}
+            observation_name = str(os.path.basename(file).split('.')[0])
+            observation['id'] = observation_name
+            observation['rdf'] = rdf_path_prefix + file
+            observation['geojson'] = {
+                'nuts1': {'path': geojson_path_prefix + "nuts_1/" + observation_name + ".geojson",
+                          'exists': observation_name in nuts_files[0]},
+                'nuts2': {'path': geojson_path_prefix + "nuts_2/" + observation_name + ".geojson",
+                          'exists': observation_name in nuts_files[1]},
+                'nuts3': {'path': geojson_path_prefix + "nuts_3/" + observation_name + ".geojson",
+                          'exists': observation_name in nuts_files[2]},
+            }
+            observation_list.append(observation)
+        return observation_list
